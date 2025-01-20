dab99579656cddc635b8e63de84b453fe78154b2
Simeon199 <sim.ki@outlook.de>
2025-01-20 10:44:47 +0100

    recent changes saved

--- src/app/main-content/skills/skills.component.ts
+++ src/app/main-content/skills/skills.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { DescriptionWrapperComponent } from '../shared/description-wrapper/description-wrapper.component';
+import { ButtonStyleComponent } from '../shared/button-style/button-style.component';
 
 @Component({
   selector: 'app-skills',
   standalone: true,
-  imports: [],
+  imports: [
+    DescriptionWrapperComponent,
+    ButtonStyleComponent
+  ],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.scss'
 })
