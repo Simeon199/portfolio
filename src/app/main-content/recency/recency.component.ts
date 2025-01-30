3448d271618c5a8cd1bde10b505a00219769d226
Simeon199 <sim.ki@outlook.de>
2025-01-30 12:57:13 +0100

    slide animation in recency.component now works properly

--- src/app/main-content/recency/recency.component.ts
+++ src/app/main-content/recency/recency.component.ts
@@ -18,7 +18,7 @@ export class RecencyComponent {
   isAnimating = false;
   currentTranslateX = 0;
   commentWidth = 636;
-
+  transitionEnabled = true;
   recencies: recencies = {
     recency1: 'I had the good fortune of working with Lukas in a group project at the Developer Akademie that involved a lot of effort. He always stayed calm, cool and focused, and made sure our team was set up for success. He is super knowledgeable, easy to work with, an I would happily work with him again given the chance',
     recency2: 'Our project benefited enormously from Lukas efficient way of working.',
@@ -36,47 +36,52 @@ export class RecencyComponent {
   allItemValues = this.getKeys();
   allRecencyValues = this.getValues();
 
-
-  // navigateFunction(direction: string) {
-  //   let initializeRecencyOne = this.allRecencyValues[0];
-  //   let initializeRecencyTwo = this.allRecencyValues[1];
-  //   let initializeRecencyThree = this.allRecencyValues[2];
-  //   if (direction == 'right') {
-  //     setTimeout(() => {
-  //       this.allRecencyValues[0] = initializeRecencyThree;
-  //       this.allRecencyValues[1] = initializeRecencyOne;
-  //       this.allRecencyValues[2] = initializeRecencyTwo;
-  //     }, 500);
-  //   } else if (direction == 'left') {
-  //     setTimeout(() => {
-  //       this.allRecencyValues[0] = initializeRecencyTwo;
-  //       this.allRecencyValues[1] = initializeRecencyThree;
-  //       this.allRecencyValues[2] = initializeRecencyOne;
-  //     }, 500);
-  //   }
-  // }
-
   navigateFunction(direction: string) {
+    this.initializeVariables();
+    this.currentTranslateX += this.determineCorrectShiftAmount(direction);
+    setTimeout(() => {
+      this.permuteRecencyArrayDependingOnMovingDirection(direction);
+      this.setVariablesBackToOriginalValues();
+    }, this.animationDuration);
+  }
+
+  determineCorrectShiftAmount(direction: string) {
+    let shiftAmount = 0;
+    if (direction == 'right') {
+      return shiftAmount = -this.commentWidth;
+    } else {
+      return shiftAmount = this.commentWidth;
+    }
+  }
+
+  setVariablesBackToOriginalValues() {
+    this.currentTranslateX = 0;
+    setTimeout(() => {
+      this.transitionEnabled = true;
+      this.isAnimating = false;
+    }, 50);
+  }
+
+  initializeVariables() {
     if (this.isAnimating) {
       return;
     }
     this.isAnimating = true;
-    const shiftAmount = direction === 'right' ? -this.commentWidth : this.commentWidth
-    this.currentTranslateX += shiftAmount;
-    setTimeout(() => {
-      if (direction === 'right') {
-        const firstItem = this.allRecencyValues.shift();
-        if (firstItem) {
-          this.allRecencyValues.push(firstItem);
-        }
-      } else {
-        const lastItem = this.allRecencyValues.pop();
-        if (lastItem) {
-          this.allRecencyValues.unshift(lastItem);
-        }
+    this.transitionEnabled = true;
+  }
+
+  permuteRecencyArrayDependingOnMovingDirection(direction: string) {
+    this.transitionEnabled = false;
+    if (direction === 'right') {
+      let firstItem = this.allRecencyValues.shift();
+      if (firstItem) {
+        this.allRecencyValues.push(firstItem);
       }
-      this.currentTranslateX = 0;
-      this.isAnimating = false;
-    }, this.animationDuration);
+    } else {
+      let lastItem = this.allRecencyValues.pop();
+      if (lastItem) {
+        this.allRecencyValues.unshift(lastItem);
+      }
+    }
   }
 }
