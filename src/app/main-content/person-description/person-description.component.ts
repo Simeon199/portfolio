98675e2623bc8764494e7d3e49592020508f0f63
Simeon199 <sim.ki@outlook.de>
2025-02-01 18:10:51 +0100

    several changes added

--- src/app/main-content/person-description/person-description.component.ts
+++ src/app/main-content/person-description/person-description.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { DescriptionWrapperComponent } from '../shared/description-wrapper/description-wrapper.component';
+import { NgOptimizedImage } from '@angular/common';
 
 
 @Component({
   selector: 'app-person-description',
   standalone: true,
-  imports: [DescriptionWrapperComponent],
+  imports: [
+    DescriptionWrapperComponent,
+    NgOptimizedImage
+  ],
   templateUrl: './person-description.component.html',
   styleUrl: './person-description.component.scss'
 })
--- src/app/main-content/projects/single-project-faded-in/single-project-faded-in.component.ts
+++ src/app/main-content/projects/single-project-faded-in/single-project-faded-in.component.ts
@@ -1,10 +1,14 @@
 import { Component, Input } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { NgOptimizedImage } from '@angular/common'
 
 @Component({
   selector: 'app-single-project-faded-in',
   standalone: true,
-  imports: [],
+  imports: [
+    NgOptimizedImage,
+    CommonModule
+  ],
   templateUrl: './single-project-faded-in.component.html',
   styleUrl: './single-project-faded-in.component.scss'
 })
