0e29f72e6b9b74bf5ab532944879c369bb4eb4c9
Simeon199 <sim.ki@outlook.de>
2025-02-01 22:20:42 +0100

    recent changes saved

--- src/app/main-content/projects/projects.data.ts
+++ src/app/main-content/projects/projects.data.ts
@@ -4,7 +4,7 @@ export const TECHNOLOGY_DATA: technologyData = {
         title: 'Join',
         projectImageSource: '../../../../assets/img/join_image.png',
         description: 'What is this project about? Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
-        technologyList: ['HTML', 'CSS', 'JavaScript'],
+        technologyList: ['HTML', '|', 'CSS', '|', 'JavaScript'],
         technologyImageIcons: [
             '../../../../assets/img/html-technology.svg',
             '../../../../assets/img/css-technology.svg',
@@ -17,7 +17,7 @@ export const TECHNOLOGY_DATA: technologyData = {
         title: 'El Pollo Loco',
         projectImageSource: '../../../../assets/img/pollo_loco.png',
         description: 'Jump, run and throw game based on object-oriented approach. Help Pepe to find coins and tabasco salsa to fight against the crazy hen.',
-        technologyList: ['HTML', 'CSS', 'JavaScript'],
+        technologyList: ['HTML', '|', 'CSS', '|', 'JavaScript'],
         technologyImageIcons: [
             '../../../../assets/img/html-technology.svg',
             '../../../../assets/img/css-technology.svg',
@@ -30,7 +30,7 @@ export const TECHNOLOGY_DATA: technologyData = {
         title: 'DABubble',
         projectImageSource: '../../../../assets/img/da-bubble.png',
         description: 'This App is a Slack Clone App. It revolutionizes team communication and collaboration with its intuitive interface, real-time messaging, and robust channel organization',
-        technologyList: ['Angular', 'TypeScript', 'Firebase', 'SCSS', 'HTML'],
+        technologyList: ['Angular', '|', 'TypeScript', '|', 'Firebase', '|', 'SCSS', '|', 'HTML'],
         technologyImageIcons: [
             '../../../../assets/img/angular-technology.svg',
             '../../../../assets/img/typescript-technology.svg',
--- src/app/main-content/projects/single-project/single-project.component.ts
+++ src/app/main-content/projects/single-project/single-project.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-single-project',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './single-project.component.html',
   styleUrl: './single-project.component.scss'
 })
