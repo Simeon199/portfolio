2b2a2c295165698fbf09ccac82a407ea46e971fc
Simeon199 <sim.ki@outlook.de>
2025-02-03 22:14:57 +0100

    dropdown menu added

--- src/app/main-content/header/header.component.ts
+++ src/app/main-content/header/header.component.ts
@@ -11,12 +11,30 @@ import { CommonModule } from '@angular/common';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+
   isGermanButtonActive: boolean = false;
+  isDropdownMenuActivated: boolean = false;
+
   checkActiveButtonStyle(activateLanguage: string) {
     if (activateLanguage == 'german') {
       this.isGermanButtonActive = true;
     } else if (activateLanguage == 'english') {
       this.isGermanButtonActive = false;
     }
+  }
+
+  showOrHideDropdown() {
+    if (window.innerWidth <= 1350) {
+      if (this.isDropdownMenuActivated == false) {
+        this.isDropdownMenuActivated = true;
+      } else if (this.isDropdownMenuActivated == true) {
+        this.isDropdownMenuActivated = false;
+      }
+    }
+    console.log('function is triggered');
+  }
+
+  stopEventPropagation(event: Event) {
+    event.stopPropagation();
   }
 }
