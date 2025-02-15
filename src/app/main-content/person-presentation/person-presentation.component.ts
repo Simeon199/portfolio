88fc2b386086676015e00c575df98a16e0df0ada
Simeon199 <sim.ki@outlook.de>
2025-02-15 21:14:28 +0100

    translation function of person-presentation component completed

--- src/app/main-content/person-presentation/person-presentation.component.ts
+++ src/app/main-content/person-presentation/person-presentation.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
 import { ButtonStyleComponent } from '../shared/button-style/button-style.component';
+import { TranslateModule } from '@ngx-translate/core';
+import { LanguageService } from '../../language.service';
 
 @Component({
   selector: 'app-person-presentation',
   standalone: true,
-  imports: [ButtonStyleComponent],
+  imports: [ButtonStyleComponent, TranslateModule],
   templateUrl: './person-presentation.component.html',
   styleUrl: './person-presentation.component.scss'
 })
-export class PersonPresentationComponent { }
+export class PersonPresentationComponent {
+  currentLanguage: string = "de";
+  constructor(private languageService: LanguageService) {
+    this.languageService.currentLanguage$.subscribe(lang => {
+      this.currentLanguage = lang;
+    });
+  }
+}
