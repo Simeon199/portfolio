eec586b7a8983262f0701f08870ffa3aa8e5445d
Simeon199 <sim.ki@outlook.de>
2025-02-15 21:27:33 +0100

    translation in skills component completed

--- src/app/main-content/skills/skills.component.ts
+++ src/app/main-content/skills/skills.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { ButtonStyleComponent } from '../shared/button-style/button-style.component';
+import { TranslateModule } from '@ngx-translate/core';
+import { LanguageService } from '../../language.service';
 
 @Component({
   selector: 'app-skills',
   standalone: true,
-  imports: [ButtonStyleComponent],
+  imports: [ButtonStyleComponent, TranslateModule],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.scss'
 })
 export class SkillsComponent {
-
+  currentLanguage: string = "de";
+  constructor(private languageService: LanguageService) {
+    this.languageService.currentLanguage$.subscribe(lang => {
+      this.currentLanguage = lang;
+    });
+  }
 }
