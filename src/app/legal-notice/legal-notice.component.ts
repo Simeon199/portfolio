31df6285a4381401f196a837b91b00742b934237
Simeon199 <sim.ki@outlook.de>
2025-02-20 21:36:55 +0100

    formatting legal notice

--- src/app/legal-notice/legal-notice.component.ts
+++ src/app/legal-notice/legal-notice.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../main-content/header/header.component';
+import { FooterComponent } from '../main-content/footer/footer.component';
 
 @Component({
   selector: 'app-legal-notice',
   standalone: true,
-  imports: [],
+  imports: [
+    HeaderComponent,
+    FooterComponent
+  ],
   templateUrl: './legal-notice.component.html',
   styleUrl: './legal-notice.component.scss'
 })
