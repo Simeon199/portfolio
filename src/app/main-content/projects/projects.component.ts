0df93d00badc5280a0fdaf4f3d7c07c71f68e55b
Simeon199 <sim.ki@outlook.de>
2025-02-23 17:30:23 +0100

    changes saved related to styling

--- src/app/main-content/projects/projects.component.ts
+++ src/app/main-content/projects/projects.component.ts
@@ -95,7 +95,7 @@ export class ProjectsComponent {
       this.hoveredProjectLocation = "align-self: flex-start";
     } else if (currentHoveredProject == "El Pollo Loco") {
       this.hoveredProjectLocation = "align-self: center";
-    } else if (currentHoveredProject == "DABubble") {
+    } else if (currentHoveredProject == "DABubble (Coming Soon)") {
       this.hoveredProjectLocation = "align-self: flex-end";
     }
   }
