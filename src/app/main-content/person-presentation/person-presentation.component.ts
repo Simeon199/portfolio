84d8317b5df9892c131bb0b829f9543d73e740bb
Simeon199 <sim.ki@outlook.de>
2025-03-03 22:40:27 +0100

    saved changes added

--- src/app/main-content/person-presentation/person-presentation.component.ts
+++ src/app/main-content/person-presentation/person-presentation.component.ts
@@ -2,19 +2,44 @@ import { Component } from '@angular/core';
 import { ButtonStyleComponent } from '../shared/button-style/button-style.component';
 import { TranslateModule } from '@ngx-translate/core';
 import { LanguageService } from '../../language.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-person-presentation',
   standalone: true,
-  imports: [ButtonStyleComponent, TranslateModule],
+  imports: [
+    ButtonStyleComponent,
+    TranslateModule,
+    CommonModule
+  ],
   templateUrl: './person-presentation.component.html',
   styleUrl: './person-presentation.component.scss'
 })
+
 export class PersonPresentationComponent {
+
+  isGithubIconHovered: boolean = false;
+  isMailIconHovered: boolean = false;
+  isLinkedinIconHovered: boolean = false;
+
+  isHovered: boolean = false;
   currentLanguage: string = "de";
+
   constructor(private languageService: LanguageService) {
     this.languageService.currentLanguage$.subscribe(lang => {
       this.currentLanguage = lang;
     });
+  }
+
+  toggleHover(state: boolean, icon: string) {
+    // debugger;
+    if (icon == 'mail') {
+      this.isMailIconHovered = state;
+    } else if (icon == 'github') {
+      this.isGithubIconHovered = state;
+    } else if (icon == 'linkedin') {
+      this.isLinkedinIconHovered = state;
+    }
+    // this.isHovered = state;
   }
 }
