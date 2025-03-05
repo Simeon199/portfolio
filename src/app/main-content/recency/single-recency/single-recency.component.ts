37dba5c8e22f0f62cc27de1d6caec349956b4bf3
Simeon199 <sim.ki@outlook.de>
2025-03-05 21:30:48 +0100

    more colors added to animation in recency component section

--- src/app/main-content/recency/single-recency/single-recency.component.ts
+++ src/app/main-content/recency/single-recency/single-recency.component.ts
@@ -1,13 +1,15 @@
 import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-single-recency',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './single-recency.component.html',
   styleUrl: './single-recency.component.scss'
 })
 export class SingleRecencyComponent {
   @Input() recency!: string;
   @Input() recencyAuthor!: string;
+  @Input() index!: number;
 }
