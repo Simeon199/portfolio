f01f784d573aa3547d52f90c093a8a786e592780
Simeon199 <sim.ki@outlook.de>
2025-03-06 12:59:11 +0100

    old project images replaced by new ones (still unstyled)

--- src/app/main-content/projects/projects.data.ts
+++ src/app/main-content/projects/projects.data.ts
@@ -36,7 +36,7 @@ export const projectsData: projectsModel = {
     },
     daBubble: {
         title: 'DABubble (Coming Soon)',
-        projectImageSource: '../../../../assets/img/dabubble.svg',
+        projectImageSource: '../../../../assets/img/dabubble.png',
         description: 'In Anlehnung an Slack bietet diese App eine benutzerfreundliche Plattform zur Optimierung der Teamkommunikation. Durch Echtzeitnachrichten und eine strukturierte Kanalorganisation wird der Austausch im Team spürbar vereinfacht.',
         technologyList: ['Angular', '|', 'TypeScript', '|', 'Firebase', '|', 'SCSS', '|', 'HTML'],
         technologyImageIcons: [
